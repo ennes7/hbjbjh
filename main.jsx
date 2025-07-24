f4a367aa1e51b267afe71e8fa0585c7829901805
ennes7 <emg.pakos@gmail.com>
2025-07-24 16:12:55 +0200

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import ChatbotPrototype from "./ChatbotPrototype";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <ChatbotPrototype />
+  </React.StrictMode>
+);
